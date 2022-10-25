3c784769bf1497ae891c1e8096270a1606adda0a
indianous <36607540+indianous@users.noreply.github.com>
2022-10-25 17:48:57 -0300

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return <div className={styles.container}>Hello World!</div>;
+  return <div className={styles.container}>Hello World2!</div>;
 }
